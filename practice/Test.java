ae036a2a15ada3c0da6dc4ba659b5bbda0893d1f
sudhi <sid4it@gmail.com>
2014-11-17 01:40:14 -0600

    Initial Checkin

---
+++ practice/Test.java
@@ -0,0 +1,31 @@
+import System.out.*;
+
+class Test
+{
+	/**
+	 * Constructor
+	 *
+	 */
+	public Test()
+	{
+		int i = 20; 
+		if(i > 15)
+		{
+			return i;
+		}
+	}
+	
+	/**
+	 * Gets data from the db
+	 * @return String
+	 */
+	 public String getData()
+	 {
+	 
+	 }
+	
+	
+	
+	
+
+}
